8141a765288f8cae68e2993595be7740086e0310
William Cuc <williamcuc@MacBook-Pro-de-William.local>
2017-06-01 09:57:01 -0600

    Creación de Menu de Aplicación

---
+++ TagDesigner/TagDesigner/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TagDesigner
+//
+//  Created by William Cuc on 1/06/17.
+//  Copyright © 2017 William Cuc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.addSlideMenuButton()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
