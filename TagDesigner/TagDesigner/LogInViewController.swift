32b94a33eedac673ca9ac9e7001faaac761144f4
edgarvega <edgarvega54@gmail.com>
2017-06-26 15:25:28 -0600

    coneccion firebase

--- TagDesigner/TagDesigner/LogInViewController.swift
+++ TagDesigner/TagDesigner/LogInViewController.swift
@@ -6,35 +6,64 @@
 //  Copyright © 2017 William Cuc. All rights reserved.
 //
 import FBSDKLoginKit
-import FBSDKCoreKit
 import UIKit
-import FacebookLogin
+import Firebase
 
 class LogInViewController: ViewController, FBSDKLoginButtonDelegate {
+    
+    var LoginButton = FBSDKLoginButton()
 
+    
     @IBAction func logIn(_ sender: Any) {
         
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         let loginButton = FBSDKLoginButton()
         view.addSubview(loginButton)
         loginButton.frame = CGRect(x: 16, y: 425, width: view.frame.width - 32, height: 30)
-        
         loginButton.delegate = self
+        loginButton.readPermissions = ["email", "public_profile"]
+        
+        
     }
     
     func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
         print("Salir de Facebook")
     }
+    
     func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
         if error != nil{
             print(error)
             return
         }
-        print("Logueo Correcto")
+        
+        let accesToken = FBSDKAccessToken.current()
+        guard let accessTokenString = accesToken?.tokenString else {return}
+        let credential = FacebookAuthProvider.credential(withAccessToken: accessTokenString)
+        Auth.auth().signIn(with: credential, completion: {(user,error) in
+            if error != nil {
+                print("something  went  wrong  with  our Fb user:", error ?? "")
+                return
+            }
+            print("successfully  logged int with our user ", user ?? "")
+            
+        })
+        
+        
+        FBSDKGraphRequest(graphPath: "/me", parameters: ["fields": "id,name,email,gender"]).start {(connection, result,err) in
+            if err != nil{
+                print("failed to start graph request:", err ?? "")
+                return
+            }
+            print(result ?? "")
+        }
     }
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
